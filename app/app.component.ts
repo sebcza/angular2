7fb35cfc4e4dbe46ae59ec75d4a53aecf0c5a650
Sebastian Czarnecki <sebcza@outlook.com>
2016-05-29 17:09:34 +0200

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from 'angular2/core';
+import { HTTP_PROVIDERS } from 'angular2/http';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import { DashboardComponent } from './hs-search-dashboard/hs-search-dashboard.component';
+
+@Component({
+  selector: 'hearthstone-app',
+  template: `
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <router-outlet></router-outlet>
+    `,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [HTTP_PROVIDERS, ROUTER_PROVIDERS]
+})
+@RouteConfig([
+  { path: '/dashboard', as: 'Dashboard', component: DashboardComponent, useAsDefault: true },
+])
+export class AppComponent { }
---
+++ app/hs-search-dashboard/hs-search-dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'hs-search-dashboard',
+	templateUrl: 'app/hs-search-dashboard/hs-search-dashboard.component.html'
+})
+export class DashboardComponent {
+
+}
