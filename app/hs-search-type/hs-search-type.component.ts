1e7a26337620448ef4d8754511bc518d2436b7eb
Sebastian Czarnecki <sebcza@outlook.com>
2016-05-29 18:24:40 +0200

    Added hs-search-type component

---
+++ app/hs-search-type/hs-search-type.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: 'hs-search-type',
+    templateUrl: 'app/hs-search-type/hs-search-type.component.html'
+})
+export class HsSearchType {
+    
+}
--- app/hs-search-types/hs-search-types.component.ts
+++ app/hs-search-types/hs-search-types.component.ts
@@ -1,8 +1,10 @@
 import {Component} from "angular2/core";
+import {HsSearchType} from "../hs-search-type/hs-search-type.component";
 
 @Component({
     selector: 'hs-search-types',
-    templateUrl: 'app/hs-search-types/hs-search-types.component.html'
+    templateUrl: 'app/hs-search-types/hs-search-types.component.html',
+    directives: [HsSearchType]
 })
 export class HsSearchTypes {
     
