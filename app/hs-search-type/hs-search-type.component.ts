c4da61271d9a2cbed546fa1037129add9423e7b9
Sebastian Czarnecki <sebcza@outlook.com>
2016-06-02 20:58:05 +0200

    search type component

--- app/hs-search-type/hs-search-type.component.ts
+++ app/hs-search-type/hs-search-type.component.ts
@@ -1,9 +1,29 @@
-import {Component, Input} from "angular2/core";
+import {Component, Input, Output, EventEmitter} from "angular2/core";
+import {
+    FORM_DIRECTIVES, CORE_DIRECTIVES, FormBuilder, Validators, ControlGroup,
+    AbstractControl
+} from "angular2/common";
 
 @Component({
     selector: 'hs-search-type',
-    templateUrl: 'app/hs-search-type/hs-search-type.component.html'
+    templateUrl: 'app/hs-search-type/hs-search-type.component.html',
+    directives:[FORM_DIRECTIVES, CORE_DIRECTIVES]
 })
 export class HsSearchType {
     @Input() items: string[];
+    @Output() itemChosen: EventEmitter<string> = new EventEmitter<string>();
+    typeForm: ControlGroup;
+    type: AbstractControl;
+
+    constructor(fb: FormBuilder) {
+        this.typeForm = fb.group({
+            'type': ['', Validators.required]
+        });
+
+        this.type = this.typeForm.controls['type'];
+    }
+
+    onSubmit(value: any): void {
+        this.itemChosen.emit(value.type);
+    }
 }
--- app/hs-search-types/hs-search-types.component.ts
+++ app/hs-search-types/hs-search-types.component.ts
@@ -2,18 +2,21 @@ import {Component, OnInit} from "angular2/core";
 import {HsSearchType} from "../hs-search-type/hs-search-type.component";
 import {ICardInfo} from "../cards/card-info";
 import {CardInfoService} from "../cards/card-info.service";
+import {FORM_DIRECTIVES} from "angular2/common";
 
 @Component({
     selector: 'hs-search-types',
     templateUrl: 'app/hs-search-types/hs-search-types.component.html',
-    directives: [HsSearchType],
+    directives: [HsSearchType, FORM_DIRECTIVES],
     providers: [CardInfoService]
 })
 export class HsSearchTypes implements OnInit {
     cardsInfo: ICardInfo;
     errorMessage: string;
 
-    constructor(private _cardInfoService: CardInfoService) {}
+    constructor(private _cardInfoService: CardInfoService) {
+
+    }
 
     ngOnInit(): void {
         this._cardInfoService.getInfo()
@@ -25,4 +28,7 @@ export class HsSearchTypes implements OnInit {
             )
     }
 
+    searchItems(item: string): void {
+        console.log('search items ', item);
+    }
 }
