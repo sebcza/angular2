0b1dbd2dba1a590ab11849ac5d3bea8cabb60864
Sebastian Czarnecki <sebcza@outlook.com>
2016-06-04 12:43:05 +0200

    routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,13 @@ import { HTTP_PROVIDERS } from 'angular2/http';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
 import { HsSearchDashboardComponent } from './hs-search-dashboard/hs-search-dashboard.component';
+import {HsAllCardsComponent} from "./hs-all-cards/hs-all-cards.component";
 
 @Component({
   selector: 'hearthstone-app',
   template: `
     <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['All']">All cards</a>
     <router-outlet></router-outlet>
     `,
   directives: [ROUTER_DIRECTIVES],
@@ -15,5 +17,6 @@ import { HsSearchDashboardComponent } from './hs-search-dashboard/hs-search-dash
 })
 @RouteConfig([
   { path: '/dashboard', as: 'Dashboard', component: HsSearchDashboardComponent, useAsDefault: true },
+  { path: '/all', as: 'All', component: HsAllCardsComponent}
 ])
 export class AppComponent { }
---
+++ app/hs-all-cards/hs-all-cards.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: 'hs-all-cards',
+    templateUrl: 'app/hs-all-cards/hs-all-cards.component.html'
+})
+export class HsAllCardsComponent {
+
+}
