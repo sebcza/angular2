2ebba662f28945562c6c06cd73c68b9b09868ddc
Sebastian Czarnecki <sebcza@outlook.com>
2016-06-04 13:47:58 +0200

    display cards searched by name

--- app/hs-all-cards/hs-all-cards.component.ts
+++ app/hs-all-cards/hs-all-cards.component.ts
@@ -1,14 +1,17 @@
 import {Component} from "angular2/core";
 import {HsSearchAll} from "../hs-search-all/hs-search-all.component";
+import {ICard} from "../cards/card";
+import {HsCardsTable} from "../hs-cards-table/hs-cards-table.component";
 
 @Component({
     selector: 'hs-all-cards',
     templateUrl: 'app/hs-all-cards/hs-all-cards.component.html',
-    directives: [HsSearchAll]
+    directives: [HsSearchAll, HsCardsTable]
 })
 export class HsAllCards {
+    cards: ICard[];
 
-    setCards(cards) {
-        console.log('cards ', cards);
+    setCards(cards: ICard[]) {
+        this.cards = cards;
     }
 }
